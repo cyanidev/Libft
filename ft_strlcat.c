319a4b81d9b0520913b2f54287da90822b4db2c6
Andrea Feliciani <93842463+cyanidev@users.noreply.github.com>
2023-02-27 16:43:53 +0100

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -12,27 +12,21 @@
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dest, const char *src, size_t size)
-{
-	unsigned int	i;
-	unsigned int	dest_len;
-	unsigned int	src_len;
-	char			*srca;
+size_t	ft_strlcat(char *dest, const char *src, size_t destsize)
+{	
+	size_t	i;
+	size_t	lendest;
 
-	srca = (char *)src;
 	i = 0;
-	dest_len = ft_strlen(dest);
-	src_len = ft_strlen((char *)srca);
-	if (size > dest_len)
+	lendest = ft_strlen(dest);
+	if (!(destsize == 0 || lendest > destsize))
 	{
-		while (src[i] && dest_len + i < size - 1)
+		while (destzize - lendest - 1 > i)
 		{
-			dest[dest_len + i] = src[i];
+			dest[lendest + i] = src[i];
 			i++;
 		}
-		dest[dest_len + i] = '\0';
-		return (src_len + dest_len);
+		dest[lendest + i] = '\0';
 	}
-	else
-		return (src_len + size);
+	return (lendest + ft_strlen(src));
 }
